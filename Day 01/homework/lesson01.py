4e9d401edb31a6f30c88e3b8bf1184f18cc2c768
begear212 <aberothkiller31@gmail.com>
2025-07-04 18:53:36 +0400

    day 01

---
+++ Day 01/homework/lesson01.py
@@ -0,0 +1,22 @@
+print("blue")
+print("ბებოს კარტოფილი")
+print("playing football")
+
+# favorite_film = "tulsa king"
+# favorite_film = "avengers endgame"
+# favorite_film = "turman show"
+# favorite_film = "jhon wick"
+# favorite_film = "doctor strange"
+
+# კონაკტინაცია ნიშნავს string-ების შეერთებას
+
+# ცვლადი
+# მონაცემთა ტიპები
+# ციფრული მონაცემები
+# კონკატინაცია
+
+
+
+# name = "ნიკა"
+# age = "12"
+print("ჩემი სახელი არის " + " ნიკა " + " და ჩემი ასაკია " + " 12 " )
