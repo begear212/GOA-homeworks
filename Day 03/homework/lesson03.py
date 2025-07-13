4b6d78d3c6da0e9431a38193f1dfae7c12901e19
begear212 <aberothkiller31@gmail.com>
2025-07-13 19:52:14 +0400

    Day 03

---
+++ Day 03/homework/lesson03.py
@@ -0,0 +1,33 @@
+#username = nick
+print("hello " + "nick")
+
+# input არის ფუნქცია რომლის შიგნით () იწერება კითხბები string ის სახით
+
+#update print
+
+click = input("hello fellow adventurer")
+print("input")
+print("input")
+
+#update print 
+interact = input("hello adventurer")
+print("interact")
+answer = input("hello")
+print("answer")
+
+#update answer
+#update print
+more = input("you have avoided an a big danger do you understand that advantuere?")
+print("more")
+answer = input("it is understandable")
+
+#update print
+#update more
+#update answer
+more = input("would you like to continue your adventure by doing a quest but you have to be careful do you understand that?")
+print("more")
+answer = input("it is understandable")
+print("answer")
+#update print
+#update more
+#update answer
