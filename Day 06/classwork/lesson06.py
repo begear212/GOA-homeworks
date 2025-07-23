0ad1e9adadbdd879f86957b96d4e06d12591aa5c
begear212 <aberothkiller31@gmail.com>
2025-07-23 14:06:02 +0400

    Day 06

---
+++ Day 06/classwork/lesson06.py
@@ -0,0 +1,23 @@
+hi="გამარჯობა"
+greetings="გაგიმარჯოთ"
+print(hi+greetings)
+
+name = "ნიკა"
+message = "შრეკი მიყვარს"
+
+age = 12
+birthday = 2012
+
+height = 172.7
+shrek_height = 228,6
+
+shrek_is_the_best_animation_ever = True
+shre_is_the_worst_comedy_ever = False
+
+me = "12"
+age = int(me)
+
+height = "172.7"
+my_height = str(height)
+
+
---
+++ Day 06/homework/lesson06.py
@@ -0,0 +1,36 @@
+# ბულიანის გამოყენებით ჩვენ შეგვიძლია დავწეროთ true და false მაგ : Shrek_is_the_best_animation_ever = true
+
+# > ამოწმებს მარჯვენა რიცხვი მეტია თუ არა მარცხენაზე
+# < ამოწმებს მარცხენა რიცხვი მეტია თუ არა მარჯვენაზე
+
+name = "ნიკა"
+age = str("12")
+birthday = str("2012")
+favorite_movie = "shrek 2"
+favorite_animal = "dog"
+
+age = 12
+birth_year = 2012
+height = 174
+score1 = 90
+score2 = 91
+
+card_balance = 0.01
+xp_level = 98.4
+hp_bar = 48.9
+stamina_bar = 22.1
+version = 17.2
+
+shrek_is_the_best_animation = True
+i_hate_math = False
+im_psg_fan = True
+psg_won_ucl_2025 = True
+my_favorite_player_is_marquinhos = True
+
+shrek1 = input(10/10)
+shrek2 = input(8.5/10)
+shrek1 > shrek2
+
+
+on = True
+off = False
