498a9e8637b6d0d06d9f73f8cdc58cca1b168357
begear212 <aberothkiller31@gmail.com>
2025-07-24 23:38:01 +0400

    Day 07

---
+++ Day 07/lesson07.py
@@ -0,0 +1,58 @@
+number1 = float(input("6"))
+number2 = float(input("9"))
+
+
+# შეკითხვები და პასუხები
+print("პასუხები")
+#update print
+print("number1" + "number2") = 6+9 = 15
+#update print
+print("number1" * "number2") = 6*9 = 54
+#update print
+print("number1" / "number2") = 6/9 = 0.(6)
+#update print
+print("number2" - "number1") = 9-6 = 3
+#დანარჩენები როგორ გავაკეთო მაგას ვერ მივხვდი : // , ** , %
+
+number = int(input("X"))
+# X = უცნობი რიცხვი
+
+if number % 2 == 0
+print("ლუწი რიცხვია")
+#update print
+if number % 2 == 1
+print("კენტი რიცხვია")
+#update print
+
+
+score = int(input("99"))
+if score >= 90 and score <= 100
+print("შესანიშნავი")
+#update print
+If score >= 80 and score <=89
+print("კარგია")
+#update print
+if score >= 70 and score <= 79
+print("საშვალოა")
+#update print
+if scoore >= 60 and score <= 69
+print("ცუდია")
+#update print
+if score >= 59 and score <= 0
+print("ძალიან ცუდია")
+
+
+# მეოთხე დავალებას ვერ მივხვდი
+
+
+#update print
+if temperature >25 celsius and if wheather = sunny
+print("wear shorts and T-shirt")
+#update print
+if temperature < 10 celsius and if weather = rainy
+print("wear coat and take umbrella with you")
+#update print
+if weather is not cold or hot wear
+print("wear normal clothes")
+#update print
+# the end
